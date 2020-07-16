5a31468b24234c4008f5b64b4c376255797665c5
Matthieu Pierce <matthieu.pierce@gmail.com>
2020-07-16 13:21:11 -0700

    naive requests and responses for user registration in js

--- server.js
+++ server.js
@@ -1,8 +1,99 @@
 const express = require('express');
 
-
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@email.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'sally',
+      email: 'sally@email.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    },
+  ],
+}
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+      req.body.password === database.users[0].password) {
+        res.json('successful signin');      
+      } else {
+        res.status(400).json(`error logging in`);
+      }
+  res.json(`signin post working`)
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push(    {
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res)=> {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      return res.json(user);
+      found = true;
+    }
+  })
+    if (!found) {
+      res.status(404).json(`no such user`);
+    }
+  })
+
+  app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+      if (user.id === id) {
+        user.entries++;
+        found = true;
+        return res.json(user.entries);
+      }
+    })
+      if (!found) {
+        res.status(404).json(`no such user found`);
+      }
+
+  })
 
 app.listen(3002, ()=> {
   console.log(`server is running on port 3002`);
 });
+
+/*
+// Routes map
+
+/ --> GET = this is working
+/signin --> POST = success || fail
+/register --> POST = new user object/info
+/profile/:userId --> GET user
+/image --> PUT --> user (with updated image)
+
+*/
