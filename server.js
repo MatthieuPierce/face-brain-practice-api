f5fba01598bde255e6edcbb849366708a53d44da
Matthieu Pierce <matthieu.pierce@gmail.com>
2020-07-18 15:50:41 -0700

    hook /register to database login and users

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const app = express();
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'matthieu',
-    password : '',
+    password : 'root',
     database : 'face-brain'
   }
 });
 
-console.log(postgres.select('*').from('users'));
+db  .select('*').from('users').then(data => {
+  console.log(data);
+});
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -65,48 +67,58 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  // bcrypt.hash(password, 8, function(err, hash) {
-  //   console.log(hash);
-  // });
-  database.users.push(    {
-    id: '125',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length - 1]);
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res)=> {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      return res.json(user);
-      found = true;
+  db
+  .select('*')
+  .from('users')
+  .where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('not found')
     }
   })
-    if (!found) {
-      res.status(404).json(`no such user`);
-    }
-  })
+  .catch(err => res.status(400).json('error getting user')) 
+})
 
   app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-      if (user.id === id) {
-        user.entries++;
-        found = true;
-        return res.json(user.entries);
-      }
-    })
-      if (!found) {
-        res.status(404).json(`no such user found`);
-      }
-
+    db('users')
+      .where('id', '=', id)
+      .increment('entries', 1)
+      .returning('entries')
+      .then(entries => 
+        res.json(entries[0])
+        )
+      .catch(err => res.status(400).json('unable to put entry count'));
   })
 
   // bcrypt.hash('bacon', 8, function(err, hash) {
