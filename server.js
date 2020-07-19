33c80cf35e2b9f000a37dd9285930c82f9523779
Matthieu Pierce <matthieu.pierce@gmail.com>
2020-07-18 18:23:31 -0700

    refactor signin post to implicitly accept (req, res) etc

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const app = express();
 const knex = require('knex');
 const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
 
 const db = knex({
   client: 'pg',
@@ -23,54 +27,16 @@ app.get('/', (req, res) => {
   res.send(database.users);
 });
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      let isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials');
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
+app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) 
 })
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
-app.get('/profile/:id', (req, res)=> {
-  const { id } = req.params;
-  db
-  .select('*')
-  .from('users')
-  .where({id})
-  .then(user => {
-    if (user.length) {
-      res.json(user[0]);
-    } else {
-      res.status(400).json('not found')
-    }
-  })
-  .catch(err => res.status(400).json('error getting user')) 
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db)
 })
 
-  app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users')
-      .where('id', '=', id)
-      .increment('entries', 1)
-      .returning('entries')
-      .then(entries => 
-        res.json(entries[0])
-        )
-      .catch(err => res.status(400).json('unable to put entry count'));
-  })
+app.put('/image', (req, res) => image.handleImage(req, res, db))
 
 app.listen(3002, ()=> {
   console.log(`server is running on port 3002`);
