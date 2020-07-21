ef2f591fd5e27e315a417f4edfa3e1615ebe82c3
Matthieu Pierce <matthieu.pierce@gmail.com>
2020-07-20 21:38:12 -0700

    changing response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send(database.users);
+  res.send('Server is running');
 });
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
